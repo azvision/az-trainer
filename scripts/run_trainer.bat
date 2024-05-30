2e9ff3cf0b8ec6e5dee1dfecbb599abc61188021
Jan Austerlitz <hausterlitz@gmail.com>
2024-05-30 11:09:06 +0200

    install requirements

--- scripts/run_trainer.bat
+++ scripts/run_trainer.bat
@@ -2,6 +2,7 @@
 set "PROJECT_DIR=C:\azvision\trainer"
 set "TRAINER_SCRIPT=%PROJECT_DIR%\src\trainer.py"
 set "VENV_DIR=%PROJECT_DIR%\.venv"
+set "REQUIREMENTS_FILE=%PROJECT_DIR%\requirements.txt"
 
 REM Change directory to the project directory
 cd /d "%PROJECT_DIR%" || (
@@ -29,8 +30,16 @@ if errorlevel 1 (
     exit /b 1
 )
 
+REM Install packages from requirements.txt
+echo Installing packages from requirements.txt...
+pip install -r "%REQUIREMENTS_FILE%"
+if errorlevel 1 (
+    echo Failed to install packages. Exiting...
+    exit /b 1
+)
+
 REM Start the trainer script
 echo Starting the trainer script...
-"python" "%TRAINER_SCRIPT%"
+python "%TRAINER_SCRIPT%"
 
 exit /b 0
